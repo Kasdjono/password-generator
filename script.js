12b15acb2a58d3d40a6a464d2a00acc370f64681
Nick_Kash <nicholas.kasdjono.777@my.csun.edu>
2021-08-12 20:14:02 -0700

    figured out how to use window.confirm command for the character type algorithm

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Variables
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var specialCase = ["!","?","@","#","$","%","^","&","*"];
+var numberCase = [0,1,2,3,4,5,6,7,8,9];
+var charSelection = [];
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -10,41 +17,49 @@ function writePassword() {
   function generatePassword() {
 
     // Prompt the user to determine how many character are neede for the password
-    var passLength = prompt ("How many characters are needed for the passwrod (8-128)");
+    var passLength = window.prompt("How many characters are needed for the passwrod (8-128)");
     console.log(passLength);
 
-    if(passLength <= 128) {
-      alert("You selected " + passLength + " characters.");
-    }
-    else {
-      return undefined;
+    while(passLength < 8 || passLength > 128) {
+      var passLength = window.prompt("Remember, the password must be (8-128)");
+      console.log(passLength);
     }
 
-    // Prompt the user to determine what characters are required
-    var charType = ["lowercase", "uppercase", "numeric", "special"];
-    for(i = 0; i < charType.length; i++) {
+// window  prompt
+// will create boolean
 
-      var charSelect = prompt ("Does the password require " + charType[i] + "?  Type: yes or no");
-      console.log(charSelect.toUpperCase());
+    var result
+    result = window.confirm("Do you want to use lowercase values?");
+    console.log(result);
 
-        if(charSelect == "YES") {
-          //charUsed[]
-          alert("The password will require " + charType[i] + ".");
-        }
-        else if(charSelect == "NO") {
-          alert("The password will not require " + charType[i] + ".");
-        charType.splice(i,1);
-        }
-        else {
-          return undefined;
-        }
 
+
+// concatinate  the  arrays together in order to create the final array
+// concatinate 1 character from selected  into the garenteed array
     }
 
-  }
+  
 
   passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// if (window.confirm("Do you want to use " + charType[i] + " ?"));{
+//   console.log(variable);
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
